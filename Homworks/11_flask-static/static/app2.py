41c7074acdbb6180d3e609173a4e19d64d14e3f4
Kishi <kwijaya50@stuy.edu>
2024-09-26 10:55:19 -0400

    HW 11

--- Homworks/11_flask-static/static/app2.py
+++ Homworks/11_flask-static/static/app2.py
@@ -8,24 +8,24 @@ import random
 from flask import Flask
 app = Flask(__name__)
 
-with open('static/fixie.html', 'r') as f:
+with open('fixie.html', 'r') as f:
     html_string = f.read()
 
-'''
+
 @app.route("/")
 def hello_world():
     print("the __name__ of this module is... ")
     print(__name__)
-    return "No hablo queso!"
-
+    return html_string
 '''
 
-@app.route("/static/fixie.html")
+@app.route("static/foo.html")
 def h():
     print("the __name__ of this module is... ")
     print(__name__)
-    return str(html_string)
+    return str(random.random())
 
+'''
 
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
