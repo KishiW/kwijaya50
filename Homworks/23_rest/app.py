b23d0e5e47080e39b32c7254d2d8145bcb18398b
Kishi <kwijaya50@stuy.edu>
2024-11-21 00:21:14 -0500

    23

--- Homworks/23_rest/app.py
+++ Homworks/23_rest/app.py
@@ -7,19 +7,17 @@ import urllib.request
 from flask import Flask, render_template
 
 app = Flask(__name__)
-with open('key_nasa.txt', 'r') as file:
-    DEMO_KEY = file.read()
-
-
-with urllib.request.urlopen('https://api.nasa.gov/planetary/apod?api_key=' + DEMO_KEY) as response:
-   info = response.read()
+app.secret_key = 'wawa'
 
 
 
 
 @app.route("/")
 def main():
-    return render_template("main.html", info = info)
+    data = urllib.request.urlopen('https://api.nasa.gov/planetary/apod?api_key=56fF6PjUPBIHJZmL00z4LXc6oTakAu7y0hRihvht')
+    jsonDictionary = json.loads(data.read().decode())
+    photo = jsonDictionary["url"]
+    return render_template('main.html', photo = photo, info = jsonDictionary["explanation"])
 
 
 if __name__ == "__main__":
